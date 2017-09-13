7f7f241dcfe09f0ba8dbfe021bd014e2b325718d
Alen Bou-Haidar <alencool@gmail.com>
2017-09-13 23:24:38 +1000

    Fix direction movement bug

--- src/server/server.js
+++ src/server/server.js
@@ -39,31 +39,43 @@ class Player {
         this.y = y;
         this.vx = 0;    // velocity
         this.vy = 0;
-        this.ix = 0;    // input, -1, 0, 1
+        this.ix = 0;    // resolved input, -1, 0, 1
         this.iy = 0;
+        this.left = 0;
+        this.right = 0;
+        this.up = 0;
+        this.down= 0;
     }
 
     keydown(direction) {
         if (direction === 'up') {
             this.iy = -1;
+            this.up = -1;
         } else if (direction === 'down') {
             this.iy = 1;
+            this.down = 1;
         } else if (direction === 'left') {
-            this.ix = -1
+            this.ix = -1;
+            this.left = -1;
         } else if (direction === 'right') {
             this.ix = 1;
+            this.right = 1;
         }
     }
 
     keyup(direction) {
         if (direction === 'up') {
-            this.iy = 0;
+            this.up = 0;
+            this.iy = this.down;
         } else if (direction === 'down') {
-            this.iy = 0;
+            this.down = 0
+            this.iy = this.up;
         } else if (direction === 'left') {
-            this.ix = 0;
+            this.left = 0;
+            this.ix = this.right;
         } else if (direction === 'right') {
-            this.ix = 0;
+            this.right = 0;
+            this.ix = this.left;
         }
     }
 
