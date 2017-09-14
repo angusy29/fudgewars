218ba47d81a94a5fcd02a5abdf40209c26ef7bd9
Kelvin Li <kelvinlcf@yahoo.com.hk>
2017-09-14 17:55:47 +1000

    updated server ping interval and timeout

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
 let server = require('http').Server(app);
-let io = require('socket.io').listen(server);
+let io = require('socket.io').listen(server, {
+    pingInterval: 1000,
+    pingTimeout: 1000,
+    cookie: false
+  });
 let path = require('path');
 let dist = path.resolve(__dirname + '/../../dist');
 
