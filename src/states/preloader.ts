bff2fa6f8de96abd04401c5204c60a001e31b925
Lawrence Ling <lawrence1995@hotmail.com>
2017-10-02 16:09:02 +1100

    Add simple loading bar

--- src/states/preloader.ts
+++ src/states/preloader.ts
@@ -7,7 +7,29 @@ import * as AssetUtils from '../utils/assetUtils';
 export default class Preloader extends Phaser.State {
 
     public preload(): void {
+        this.initLoadingBar();
         AssetUtils.Loader.loadAllAssets(this.game, this.waitForSoundDecoding, this);
+    }
+
+    private initLoadingBar(): void {
+        let loadingBarHeight: number = 20;
+
+        let loadingBar = this.game.add.bitmapData(this.game.width, loadingBarHeight);
+        loadingBar.ctx.beginPath();
+        loadingBar.ctx.rect(0, 0, this.game.width, loadingBarHeight);
+        loadingBar.ctx.fillStyle = '#00FF00';
+        loadingBar.ctx.fillRect(0, 0, this.game.width, loadingBarHeight);
+
+        let loadingBarBg = this.game.add.bitmapData(this.game.width, loadingBarHeight);
+        loadingBarBg.ctx.beginPath();
+        loadingBarBg.ctx.rect(0, 0, this.game.width, loadingBarHeight);
+        loadingBarBg.ctx.fillStyle = '#FFFFFF';
+        loadingBarBg.ctx.fillRect(0, 0, this.game.width, loadingBarHeight);
+
+        let loadingBarBgSprite = this.game.add.sprite(0, this.game.width / 2, loadingBarBg);
+        let loadingBarSprite = this.game.add.sprite(0, this.game.width / 2, loadingBar);
+
+        this.load.setPreloadSprite(loadingBarSprite);
     }
 
     private waitForSoundDecoding(): void {
